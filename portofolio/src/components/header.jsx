4f528f0f497d9f42eff498d78bb5fdac78e0d6be
abyanmf <abyanmf354@gmail.com>
2024-08-22 20:06:10 +0700

    first commit

---
+++ portofolio/src/components/header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <div className="container mx-auto">
+            <div className="flex justify-between">
+                <a href="/">
+                <div>kanan</div>
+                <div>kiri</div>
+                </a>
+            </div>
+        </div>
+    );
+}
